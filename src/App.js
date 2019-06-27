8d9dc8cfe0d310c09484799a7499c3fd169af5b1
mahadi-nsu <mahadi.hassan@northsouth.edu>
2019-06-27 13:55:16 +0600

    data saved and fetched to/from localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       works : []
+    };
+    this.add = this.add.bind(this)
+  }
+
+  add()
+  {
+    var title = this.refs.title.value;
+    if(localStorage.getItem('works') == null){
+      var works = [];
+      works.push(title);
+      localStorage.setItem('works',JSON.stringify(works));
+    }else{
+      var works  = JSON.parse(localStorage.getItem('works'));
+      works.push(title);
+      localStorage.setItem('works',JSON.stringify(works));
+    }
+
+
+    this.setState({
+      works : JSON.parse(localStorage.getItem('works'))
+    })
+  }
+
+  render() {
+    console.log(this.state.works)
+    return (
+      <div>
+        <h2>TODO USING LOCAL STORAGE</h2>
+        <input type="text" placeholder="Enter a title" ref="title" />
+        <input type="button" value = "Add" onClick = {this.add}/>
+      </div>
+    );
+  }
 }
 
 export default App;
